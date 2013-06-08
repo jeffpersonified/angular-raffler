27347dadd684cf7c338eeb46d973b3a98d532f3b
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-06-07 18:50:03 -0700

    add the angular

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '3511d7286fb546504df54efe9c586cec75559a4d6452c8af34540ff138eb3dac5d535e463f5f12e80302bd0bf9cd3379e3d16e6bab5bbc21582875e01ae9888d'
